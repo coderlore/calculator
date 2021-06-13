f56dc06dc577cb6848f40c4f55af35ec33b4f4b9
coderlore <64398168+coderlore@users.noreply.github.com>
2021-06-13 13:03:51 -1000

    Added buttons. Got rid of the grid from the previous version as it did not work as intended.

--- calculator.js
+++ calculator.js
@@ -1,33 +1,3 @@
-const container = document.getElementById("container");
-
-let row = document.getElementsByClassName("row"); 
-let col = document.getElementsByClassName("col");
-
-let value = 4;
-
-function grid(value) {
-    makeRows(value);
-    makeColumns(value);
-}
-
-function makeRows(rowNum) {
-    // Creates rows
-    for (x = 0; x < rowNum; x++) {
-        let row = document.createElement("div");
-        container.appendChild(row).className = "rows";
-    };
-};
-
-function makeColumns(colNum) {
-    for (let y = 0; y < row.length; y++) {
-        for (z = 0; z < colNum; z++) {
-            let newCol = document.createElement("div");
-            row[z].appendChild(newCol).className = "col";
-        };
-    };
-};
-
-grid(value);
 // math operators 
 function add(a,b) {
     return a + b;
