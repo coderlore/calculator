6c22e4f6f41d22a911932dbef9337784272535bf
coderlore <64398168+coderlore@users.noreply.github.com>
2021-06-13 21:15:23 -1000

    Fixed container and setup flexboxes for buttons

--- calculator.js
+++ calculator.js
@@ -1,3 +1,12 @@
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click',() => {
+        display(button.value);
+        console.log(button.value);
+    });
+});
+
 // math operators 
 function add(a,b) {
     return a + b;
@@ -18,6 +27,11 @@ function divide(a,b) {
 // take an operator and 2 numbers then calls a math operator
 function operate() {
     //placeholder to evelate expressions
+}
+
+// displays the current value
+function display() {
+    //console.log()
 }
 
 console.log(divide(4,2));
