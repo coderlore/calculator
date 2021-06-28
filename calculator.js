78bcb7f8cc9a2d77b46191ae95771646ddf56258
coderlore <64398168+coderlore@users.noreply.github.com>
2021-06-28 07:41:48 -1000

    Added custom att for firstNum, added function for operate, added clear button

--- calculator.js
+++ calculator.js
@@ -6,15 +6,19 @@ const backspace = document.querySelectorAll('.backspace');
 const equals = document.querySelectorAll('.equals');
 const display = document.getElementById('display');
 
+let previousKeyType = '';
 let mathOperator = '';
 let digit = '';
+let secondNum = '';
+let firstNum = '';
 
 numbers.forEach((button) => {
     button.addEventListener('click',() => {
         //console.log(button.textContent); // what the user keys 
         // display.textContent=button.textContent;
-        if (display.textContent === '0') {
+        if (display.textContent === '0' || previousKeyType === 'operator') {
             display.textContent = button.textContent;
+            previousKeyType = '';
         } else if (display.textContent != '0') {
             display.textContent = display.textContent + button.textContent;
         }
@@ -30,9 +34,29 @@ decimal.forEach((button) => {
 operator.forEach((button) => {
     button.addEventListener('click',() => {
         //document.getElementById('display').innerHTML=button.textContent;
+        Array.from(button.parentNode.children)
+            .forEach(k => k.classList.remove('is-depressed'));
         mathOperator = button.textContent;
+        button.setAttribute('data-first-number',display.textContent);
+        firstNum = button.getAttribute('data-first-number')
         button.classList.add('is-depressed');
         button.setAttribute('data-previous-key-type', 'operator');
+        previousKeyType = button.getAttribute('data-previous-key-type');
+    });
+});
+
+clear.forEach((button) => {
+    button.addEventListener('click',() => {
+        display.textContent = 0;
+    });
+});
+
+equals.forEach((button) => {
+    button.addEventListener('click',() => {
+        Array.from(button.parentNode.children)
+            .forEach(k => k.classList.remove('is-depressed'));
+        secondNum = display.textContent;
+        operate(mathOperator, firstNum, secondNum)
     });
 });
 
@@ -46,27 +70,34 @@ function appendNum(digit) {
 
 // math operators 
 function add(a,b) {
-    return a + b;
+    return parseFloat(a) + parseFloat(b);
 }
 
 function subtract(a,b) {
-    return a - b; 
+    return parseFloat(a) - parseFloat(b); 
 }
 
 function multiply(a,b) {
-    return a * b;
+    return parseFloat(a) * parseFloat(b);
 }
 
 function divide(a,b) {
-    return a / b;
+    return parseFloat(a) / parseFloat(b);
 }
 
 // take an operator and 2 numbers then calls a math operator
-function operate(mathOperator, num1, num2) {
-    //placeholder to evelate expressions
+function operate(mathOperator, firstNum, secondNum) {
     if (mathOperator == '+') {
-        return add(num1, num2);
+        result = add(firstNum, secondNum);
+        display.textContent = result;
+    } else if (mathOperator == '-') {
+        result = subtract(firstNum, secondNum);
+        display.textContent = result;
+    } else if (mathOperator == 'x') {
+        result = multiply(firstNum, secondNum);
+        display.textContent = result; 
+    } else if (mathOperator == '/') {
+        result = divide(firstNum, secondNum);
+        display.textContent = result;
     }
 }
-
-console.log(divide(4,2));
